e1bbe030fdefb7ec21dfa6a71a86b05d099cc47f
jprieton <jprieton@gmail.com>
2019-12-08 08:06:51 -0400

    Update license

--- jpwp-toolkit.php
+++ jpwp-toolkit.php
@@ -9,18 +9,18 @@
  * Text Domain:   jpwp-toolkit
  * Domain Path:   /languages/
  *
- * JPWP Toolkit is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ * (at your option) any later version.
  *
- * JPWPT oolkit is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with JPWP Toolkit. If not, see http://www.gnu.org/licenses/gpl-3.0.txt.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
  * @package JPWPToolkit
  */
